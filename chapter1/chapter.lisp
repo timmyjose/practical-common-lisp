dbe7a70a6c053aee7add376d091fe3792e347f90
Timmy Jose <zoltan.jose@gmail.com>
2020-01-25 13:11:08 +0530

    Chapters 1 and 2 complete.

---
+++ chapter1/chapter.lisp
@@ -0,0 +1,4 @@
+;;; Chapter 1 - Introduction
+
+(defun hello-world ()
+  (format t "Hello, world~%"))
---
+++ chapter2/chapter.lisp
@@ -0,0 +1,10 @@
+;;; Chpater 2 - A tour of the REPL
+
+(defun factorial (n)
+  (labels ((f (acc n)
+	     (cond
+	       ((zerop n) acc)
+	       (t (f (* acc n) (- n 1))))))
+    (f 1 n)))
+
+
